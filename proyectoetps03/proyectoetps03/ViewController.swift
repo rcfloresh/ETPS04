1a0e0a86a3ffd4f145fef9f448c29899b1080919
Roberto Flores <2530892009@mail.utec.edu.sv>
2023-09-23 13:08:46 -0600

    20230923

---
+++ proyectoetps03/proyectoetps03/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  proyectoetps03
+//
+//  Created by Roberto Flores on 20/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var linkRegister: UILabel!
+
+    @IBOutlet weak var btnLogin: UIButton!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
