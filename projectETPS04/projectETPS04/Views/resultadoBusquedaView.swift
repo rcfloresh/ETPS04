e4a9478e2a5a3317a25fa3a5557e6097b61d047e
Roberto Flores <2530892009@mail.utec.edu.sv>
2023-12-05 22:32:26 -0600

    Completo SinLogin

--- projectETPS04/projectETPS04/Views/resultadoBusquedaView.swift
+++ projectETPS04/projectETPS04/Views/resultadoBusquedaView.swift
@@ -5,33 +5,6 @@
 //  Created by Roberto Flores on 4/12/23.
 //
 
-//import SwiftUI
-//
-//struct resultadoBusquedaView: View {
-//    var farmacias: [String]
-//
-//    var body: some View {
-//
-//        NavigationView{
-//            ZStack{
-//                bgColorStyle()
-//                VStack{
-//                    NavigationLink(destination: locationFarmacia(longitud: -89.2181911, latitud: 13.6929403)) {
-//                        List(farmacias, id: \.self) { farmacia in
-//                                                Text(farmacia)
-//                                            }
-//                      }
-//                    .navigationBarTitle("Resultado de Farmacias")
-//                }
-//            }
-//        }
-//    }
-//}
-
-
-/// NUEVO
-
-
 import SwiftUI
 import Firebase
 
